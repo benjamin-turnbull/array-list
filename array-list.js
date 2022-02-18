4edd21207644ab1d956e3c1802e8ea1b5249ca42
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-02-18 18:08:16 +1030

    wrote functionality for list to array conversion

--- array-list.js
+++ array-list.js
@@ -7,3 +7,13 @@ function arrayToList(array) {
 }
 
 console.log(arrayToList([10, 20]));
+
+function listToArray(list) {
+	let array = [];
+	for (let node = list; node; node = node.rest) {
+		array.push(node.value);
+	}
+	return array;
+}
+
+console.log(listToArray(arrayToList([10, 20])));
